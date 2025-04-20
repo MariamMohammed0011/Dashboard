7add11248fde96af8a3d02f28d8b069b927c892e
MariamMohammed0011 <maream.mohamad.0938856942@gmail.com>
2025-04-20 08:23:51 -0700

    222

--- src/page/team/Data.js
+++ src/page/team/Data.js
@@ -1,19 +1,19 @@
 
 export const rows=[
 
-{id:1,name:'Mohammed Wanlii',email:'mar@gmail.com',age:12,phone:'0999',access:'tt'},
+{id:1,name:'Mohammed Wanlii',email:'mar@gmail.com',age:12,phone:'0999',access:'Admin'},
 
-{id:2,name:'Mohammed Wanlii',email:'mar@gmail.com',age:12,phone:'0999',access:'tt'}
+{id:2,name:'Mohammed Wanlii',email:'mar@gmail.com',age:12,phone:'0999',access:'Admin'}
 ,
 
-{id:3,name:'Mohammed Wanlii',email:'mar@gmail.com',age:12,phone:'0999',access:'tt'}
+{id:3,name:'Mohammed Wanlii',email:'mar@gmail.com',age:12,phone:'0999',access:'Manager'}
 ,
 
-{id:4,name:'Mohammed Wanlii',email:'mar@gmail.com',age:12,phone:'0999',access:'tt'}
+{id:4,name:'Mohammed Wanlii',email:'mar@gmail.com',age:12,phone:'0999',access:'Manager'}
 ,
 
 
-{id:5,name:'Mohammed Wanlii',email:'mar@gmail.com',age:12,phone:'0999',access:'tt'}
+{id:5,name:'Mohammed Wanlii',email:'mar@gmail.com',age:12,phone:'0999',access:'User'}
 ,
 
 
--- src/page/team/Team.jsx
+++ src/page/team/Team.jsx
@@ -1,7 +1,9 @@
-import { Box, Typography } from '@mui/material'
+
 import { useTheme } from '@mui/material/styles';
 import { DataGrid } from '@mui/x-data-grid'
 import { rows } from './Data'
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 import AdminPanelSettingsOutlinedIcon from '@mui/icons-material/AdminPanelSettingsOutlined';
 import SecurityOutlinedIcon from '@mui/icons-material/SecurityOutlined';
 import LockOpenOutlinedIcon from '@mui/icons-material/LockOpenOutlined';
@@ -16,41 +18,52 @@ const columns =[
   {field:'email', headerName:'Email', flex:1 , editable: true,align:'center',headerAlign:'center'},
   {field:'age', headerName:'Age', flex:1 , editable: true,align:'center',headerAlign:'center'},
   {field:'phone', headerName:'Phone', flex:1 , editable: true,align:'center',headerAlign:'center'},
-  {field:'access', headerName:'Access', flex:1 , editable: true,align:'center',headerAlign:'center',
-    renderCell:({row:{access}})=>
-      {return
-        (
-        <Box sx={{p:"5px",width:'99px',borderRadius:'3px',textAlign:'center',display:'flex',justifyContent:'space-evenly',backgroundColor:theme.palette.primary.dark
-  }}>
-   
-    <AdminPanelSettingsOutlinedIcon/>
-    {/* <Typography varient='body1'>{access}</Typography> */}
- 
-  {access}
-  
-  </Box>)}},
-  
-]
-
-;
+  {field:'access', headerName:'Access', flex:1 ,align:'center',headerAlign:'center',renderCell: ({ row: { access } }) => {
+    return (
+      <Box
+        sx={{
+          m: 'auto', 
+          mt:1.5,
+          gap: '5px',
+          px: 1,
+          py:0.5,
+        
+          width: '100px',
+          borderRadius: '3px',
+          textAlign: 'center',
+          alignItems:'center',
+          display: 'flex',
+          justifyContent: 'space-evenly',
+          backgroundColor:access==='Admin'? theme.palette.primary.dark :access==='Manager'? theme.palette.secondary.dark:'#3da58a' 
+        }}
+      >
+       {access==='Admin'&& <AdminPanelSettingsOutlinedIcon fontSize='small' />} 
+       {access==='Manager'&& <SecurityOutlinedIcon fontSize='small' />} 
+       {access==='User'&& <LockOpenOutlinedIcon fontSize='small' />} 
+        <Typography variant='body1' sx={{ fontSize: '13px' }}>{access}</Typography>
+      </Box>
+    );
+  }
+  }];
+      
 
 
   return (
-    <Box sx={{ height: 400, width: '100%' }}>
+    <Box sx={{ height: 400, width: '98%' }}>
       <DataGrid
         rows={rows}
         // @ts-ignore
         columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: {
-              pageSize: 5,
-            },
-          },
-        }}
-        pageSizeOptions={[5]}
+        // initialState={{
+        //   pagination: {
+        //     paginationModel: {
+        //       pageSize: 5,
+        //     },
+        //   },
+        // }}
+        // pageSizeOptions={[5]}
         // checkboxSelection
-        disableRowSelectionOnClick
+        // disableRowSelectionOnClick
       />
     </Box>
   )
