5cdcb6662c134751a166888b7fbc7975aecbcf3e
MariamMohammed0011 <maream.mohamad.0938856942@gmail.com>
2025-04-21 22:47:15 -0700

    22

--- src/page/invoices/Invoices.jsx
+++ src/page/invoices/Invoices.jsx
@@ -1,7 +1,26 @@
+import { Box } from '@mui/material'
+import { DataGrid } from '@mui/x-data-grid'
+import { rows,columns } from './data.js'
 import React from 'react'
 
 export default function Invoices() {
   return (
-    <div>Invoices Balances</div>
+   <Box sx={{ height: 400, width: '98%',mx:'auto' }}>
+        <DataGrid
+          rows={rows}
+          // @ts-ignore
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
+            },
+          }}
+          // pageSizeOptions={[5]}
+          checkboxSelection
+          // disableRowSelectionOnClick
+        />
+      </Box>
   )
 }
---
+++ src/page/invoices/data.js
@@ -0,0 +1,27 @@
+export const columns=[
+    {field:'id', headerName:'ID', flex:1,align:'center',headerAlign:'center',width:33},
+    {field:'name', headerName:'Name', flex:1,align:'center',headerAlign:'center'},
+    {field:'phone', headerName:'Phone Number', flex:1,align:'center',headerAlign:'center'},
+    {field:'email', headerName:'Email', flex:1,align:'center',headerAlign:'center'},
+    {field:'cost', headerName:'Cost', flex:1,align:'center',headerAlign:'center'},
+    {field:'data', headerName:'Data', flex:1,align:'center',headerAlign:'center'},
+   
+               
+
+];
+export const  rows=[
+
+    {id:1,name:'rohammed',phone:'(963)997831619',email:'m@gmail.com',cost:'0912 Won Street ,Damascus',data:'New York'},
+    {id:1,name:'rohammed',phone:'(963)997831619',email:'m@gmail.com',cost:'0912 Won Street ,Damascus',data:'New York'},
+    {id:1,name:'rohammed',phone:'(963)997831619',email:'m@gmail.com',cost:'0912 Won Street ,Damascus',data:'New York'},
+    {id:1,name:'rohammed',phone:'(963)997831619',email:'m@gmail.com',cost:'0912 Won Street ,Damascus',data:'New York'},
+    {id:1,name:'rohammed',phone:'(963)997831619',email:'m@gmail.com',cost:'0912 Won Street ,Damascus',data:'New York'},
+    {id:1,name:'rohammed',phone:'(963)997831619',email:'m@gmail.com',cost:'0912 Won Street ,Damascus',data:'New York'},
+    {id:1,name:'rohammed',phone:'(963)997831619',email:'m@gmail.com',cost:'0912 Won Street ,Damascus',data:'New York'},
+    {id:1,name:'rohammed',phone:'(963)997831619',email:'m@gmail.com',cost:'0912 Won Street ,Damascus',data:'New York'},
+    {id:1,name:'rohammed',phone:'(963)997831619',email:'m@gmail.com',cost:'0912 Won Street ,Damascus',data:'New York'},
+    {id:1,name:'rohammed',phone:'(963)997831619',email:'m@gmail.com',cost:'0912 Won Street ,Damascus',data:'New York'},
+   
+
+
+];
