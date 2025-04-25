44fa86d4b7c9f2f33fbeb01e3653ccc30890984e
MariamMohammed0011 <maream.mohamad.0938856942@gmail.com>
2025-04-25 11:55:36 -0700

    222

--- src/page/map/MapComponent.jsx
+++ src/page/map/MapComponent.jsx
@@ -1,9 +1,17 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import L from "leaflet"; 
+import L from "leaflet";
+
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+  iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+  shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+});
+
 
 const MapComponent = () => {
-  const position = [33.5138, 36.2765]; // دمشق
+  const position = L.latLng(33.5138, 36.2765); // دمشق
 
   return (
     <MapContainer center={position} zoom={13} style={{ height: "400px", width: "100%" }}>
@@ -12,9 +20,7 @@ const MapComponent = () => {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
       <Marker position={position}>
-        <Popup>
-          هون الموقع.
-        </Popup>
+        <Popup>هون الموقع.</Popup>
       </Marker>
     </MapContainer>
   );
